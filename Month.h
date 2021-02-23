7d3e1d30041331dd6c00b2eea623f3615c649281
syed23abbas <59986339+syed23abbas@users.noreply.github.com>
2021-02-22 18:06:53 -0600

    Create Month.h

---
+++ Month.h
@@ -0,0 +1,15 @@
+#include <string>
+class Month
+{
+private:
+  std::string name;
+  int day;
+  int monthNum;
+  static int numDays (int month);
+public:
+    Month();
+    Month (std::string theName, int theDay, int month);
+    int dayEnds(int firstDay, int numOfDays);
+    int dayStarts(int firstDayOfYear, int monthNum);
+    void printMonth();
+};
