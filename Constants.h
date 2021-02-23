24474493de949919dcbae1501abd8fb9fe1b9574
syed23abbas <59986339+syed23abbas@users.noreply.github.com>
2021-02-22 18:07:21 -0600

    Create Constants.h

---
+++ Constants.h
@@ -0,0 +1,23 @@
+class Constants
+{
+    public:
+    const std::string monday;
+    const std::string tuesday;
+    const std::string wednesday;
+    const std::string thursday;
+    const std::string friday;
+    const std::string saturday;
+    const std::string sunday;
+    const std::string january;
+    const std::string february;
+    const std::string march;
+    const std::string april = "April";
+    const std::string may = "May";
+    const std::string june = "June";
+    const std::string july = "July";
+    const std::string august = "August";
+    const std::string september = "September";
+    const std::string october = "October";
+    const std::string november = "November";
+    const std::string december = "December";
+};
